5d88b9f768bdb4f72298aed8d4e755cc927c9fd6
Natalia <natalia.arrazola@ucb.edu.bo>
2026-08-27 10:10:54 -0400

    feat: interfaz web conectada con fizzbuzz

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,11 @@
-import sumar from "./sumador";
+import fizzbuzz from "./fizzbuzz.js";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
+const inputNumero = document.querySelector("#primer-numero");
 const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
-
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  const numero = Number.parseInt(inputNumero.value);
+  div.innerHTML = "<p>" + fizzbuzz(numero) + "</p>";
 });
