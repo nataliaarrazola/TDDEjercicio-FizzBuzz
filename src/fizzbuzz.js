6a587ebfe80ad20da2dcc934a5f9619d68ff942a
Natalia <natalia.arrazola@ucb.edu.bo>
2026-08-27 09:56:15 -0400

    feat: si ingresa 3 devuelve Fizz

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,6 +1,8 @@
 function fizzbuzz(numero) {
+  if (numero === 3) {
+    return "Fizz";
+  }
   return numero.toString();
-
 }
 
 export default fizzbuzz;
--- src/fizzbuzz.spec.js
+++ src/fizzbuzz.spec.js
@@ -2,12 +2,14 @@ import fizzbuzz from "./fizzbuzz.js";
 
 describe("FizzBuzz", () => {
   it("deberia generar el mismo numero si no sigue una regla", () => {
-  expect(fizzbuzz(2)).toEqual("2");
+    expect(fizzbuzz(2)).toEqual("2");
   });
-});
 
-describe("FizzBuzz", () => {
   it("deberia generar '1' si se ingresa 1", () => {
     expect(fizzbuzz(1)).toEqual("1");
+  });
+
+  it("deberia retornar 'Fizz' si se ingresa 3", () => {
+    expect(fizzbuzz(3)).toEqual("Fizz");
   });
 });
